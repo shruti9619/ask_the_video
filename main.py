b44bbd97afe95523da30f724803c0ec93e34e114
Shruti <shruti.personified@gmail.com>
2024-06-16 16:28:23 +0000

    feat: model test

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from langchain_openai import ChatOpenAI
-from dot_env import load_dotenv
+from dotenv import load_dotenv
 
 print(load_dotenv())
 
+model = ChatOpenAI(model="gpt-3.5-turbo-0125")
 
-model = ChatOpenAI(model="gpt-3.5")
+print(model.invoke("Hi"))
