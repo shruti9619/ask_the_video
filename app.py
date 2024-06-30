935eb7778b20ab3e6c1361577e2acc01e217f318
unknown <shruti.personified@gmail.com>
2024-06-30 22:22:16 +0530

    feat: build doc process pipeline

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+from engine import run_engine
+
 st.title("Ask the Video")
 
 st.write("A Youtube video text mining app which will help you to find the answers \
@@ -7,7 +9,7 @@ st.write("A Youtube video text mining app which will help you to find the answer
 
 st.text_input(label = "Video Id", 
               key="video_id", 
-              on_change = lambda: st.write(st.session_state.video_id),  
+              on_change = view_result(run_engine(st.session_state.video_id)),  
               placeholder= "For example FgakZw6K1QQ from the url \
               https://www.youtube.com/watch?v=FgakZw6K1QQ")
 
