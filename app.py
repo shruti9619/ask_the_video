4eca5048d8b1a47a6c1cfcb2cb9a9582eaf50753
unknown <shruti.personified@gmail.com>
2024-06-30 22:46:09 +0530

    feat: connect app pipelines'

--- app.py
+++ app.py
@@ -2,17 +2,22 @@ import streamlit as st
 
 from engine import run_engine
 
+def get_chat(response):
+
+    # stream the response
+    with st.chat_message("AI"):
+        response = st.write_stream(response)
+
 st.title("Ask the Video")
 
-st.write(
-    "A Youtube video text mining app which will help you to find the answers \
-         to your questions on the youtube videos you want to."
-)
+st.write("A Youtube video text mining app which will help you to find the answers \
+         to your questions on the youtube videos you want to.")
 
-st.text_input(
-    label="Video Id",
-    key="video_id",
-    on_change=get_chat(run_engine(st.session_state.video_id)),
-    placeholder="For example FgakZw6K1QQ from the url \
-              https://www.youtube.com/watch?v=FgakZw6K1QQ",
-)
+st.text_input(label = "Video Id", 
+              key="video_id", 
+              on_change = get_chat(run_engine(st.session_state.video_id)),  
+              placeholder= "For example FgakZw6K1QQ from the url \
+              https://www.youtube.com/watch?v=FgakZw6K1QQ")
+
+
+
