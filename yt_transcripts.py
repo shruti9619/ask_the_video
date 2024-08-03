e62faaaf5e3403994687d7ef12c309f1e81d758c
shruti <shruti>
2024-08-03 13:33:57 +0000

    feat: add research data

--- yt_transcripts.py
+++ yt_transcripts.py
@@ -4,6 +4,13 @@ from setup_configs import get_logger
 
 logger = get_logger()
 
+def process_transcript(transcript: list, num_chunk_combine:int = 4) -> list:
+    new_chunks = []
+    for i in range(0, len(transcript), num_chunk_combine):
+        new_chunks.append({"text":" ".join([chunk['text'] for chunk in transcript[i:i+num_chunk_combine]])})
+    
+    return new_chunks
+
 
 def get_transcript(video_id: str) -> list:
     logger.info(f"Getting transcript for video id: {video_id}")
@@ -13,7 +20,10 @@ def get_transcript(video_id: str) -> list:
             f"Transcript for video id: {video_id} with length {len(transcript)} \
                      fetched successfully"
         )
+
+        transcript = process_transcript(transcript= transcript)
         return transcript
+        
     except Exception as e:
         logger.error(f"Error while fetching transcript for video id: {video_id}")
         logger.error(e)
